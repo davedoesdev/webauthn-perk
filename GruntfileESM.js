301d4b7bdd17069b25d0498a6daa43402fe449af
David Halls <dahalls@gmail.com>
2018-07-21 08:16:16 +0100

    Fix coverage with ES modules

--- GruntfileESM.js
+++ GruntfileESM.js
@@ -23,7 +23,7 @@ export default function (grunt) {
             test: 'node -r esm ./node_modules/.bin/wdio',
             wdio_cleanup: './test/wdio_cleanup.sh',
 
-            cover: `${nyc_path} -x Gruntfile.js -x wdio.conf.js -x 'test/**' node ${grunt_path} test`,
+            cover: `${nyc_path} --require esm -x wdio.conf.js -x 'test/**' ./node_modules/.bin/wdio`,
             cover_report: `${nyc_path} report -r lcov`,
             cover_check: `${nyc_path} check-coverage --statements 100 --branches 100 --functions 100 --lines 100`
         }
